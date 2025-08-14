e15551c3a0875c0e7d2b01f16a958303dc5beed5
rao107 <54015293+rao107@users.noreply.github.com>
2025-08-14 00:41:07 -0700

    code golf with macro

--- MissionariesCannibals.lean
+++ MissionariesCannibals.lean
@@ -63,69 +63,26 @@ def step10 : State := { m_left := 0, c_left := 2, boat_on_left := true }
 -- goal State, all missionaries and cannibals on the right
 def goal : State := { m_left := 0, c_left := 0, boat_on_left := false }
 
-lemma init_valid : valid_state init := by unfold valid_state bank_safe init m_right; simp
-lemma step1_valid : valid_state step1 := by unfold valid_state bank_safe step1 m_right; simp
-lemma step2_valid : valid_state step2 := by unfold valid_state bank_safe step2 m_right; simp
-lemma step3_valid : valid_state step3 := by unfold valid_state bank_safe step3 m_right; simp
-lemma step4_valid : valid_state step4 := by unfold valid_state bank_safe step4 m_right; simp
-lemma step5_valid : valid_state step5 := by unfold valid_state bank_safe step5 m_right c_right; simp
-lemma step6_valid : valid_state step6 := by unfold valid_state bank_safe step6 m_right c_right; simp
-lemma step7_valid : valid_state step7 := by unfold valid_state bank_safe step7 m_right c_right; simp
-lemma step8_valid : valid_state step8 := by unfold valid_state bank_safe step8 m_right c_right; simp
-lemma step9_valid : valid_state step9 := by unfold valid_state bank_safe step9 m_right c_right N; simp
-lemma step10_valid : valid_state step10 := by unfold valid_state bank_safe step10 m_right c_right; simp
-lemma goal_valid : valid_state goal := by unfold valid_state bank_safe goal m_right c_right; simp
+macro "validateState " state:ident : tactic =>
+  `(tactic| try unfold valid_state bank_safe $state m_right c_right N; simp)
 
-lemma prove_step (s_valid : valid_state s) (t_valid : valid_state t) (s_t_move : valid_move s t) : step s t := by
-  unfold step
-  constructorm* _ ∧ _, _ ∧ _
-  · exact s_valid
-  · exact t_valid
-  · exact s_t_move
+macro "validateMove " s:ident t:ident : tactic =>
+  `(tactic| try unfold valid_move $s $t; simp)
 
-theorem init_to_step1 : step init step1 := by
-  apply prove_step init_valid step1_valid
-  unfold valid_move init step1; simp
+macro "validateStep " s:ident t:ident : tactic =>
+  `(tactic| try unfold step; constructorm* _ ∧ _, _ ∧ _; validateState $s; validateState $t; validateMove $s $t)
 
-theorem step1_to_step2 : step step1 step2 := by
-  apply prove_step step1_valid step2_valid
-  unfold valid_move step1 step2; simp
-
-theorem step2_to_step3 : step step2 step3 := by
-  apply prove_step step2_valid step3_valid
-  unfold valid_move step2 step3; simp
-
-theorem step3_to_step4 : step step3 step4 := by
-  apply prove_step step3_valid step4_valid
-  unfold valid_move step3 step4; simp
-
-theorem step4_to_step5 : step step4 step5 := by
-  apply prove_step step4_valid step5_valid
-  unfold valid_move step4 step5; simp
-
-theorem step5_to_step6 : step step5 step6 := by
-  apply prove_step step5_valid step6_valid
-  unfold valid_move step5 step6; simp
-
-theorem step6_to_step7 : step step6 step7 := by
-  apply prove_step step6_valid step7_valid
-  unfold valid_move step6 step7; simp
-
-theorem step7_to_step8 : step step7 step8 := by
-  apply prove_step step7_valid step8_valid
-  unfold valid_move step7 step8; simp
-
-theorem step8_to_step9 : step step8 step9 := by
-  apply prove_step step8_valid step9_valid
-  unfold valid_move step8 step9; simp
-
-theorem step9_to_step10 : step step9 step10 := by
-  apply prove_step step9_valid step10_valid
-  unfold valid_move step9 step10; simp
-
-theorem step10_to_goal : step step10 goal := by
-  apply prove_step step10_valid goal_valid
-  unfold valid_move step10 goal; simp
+theorem init_to_step1 : step init step1 := by validateStep init step1
+theorem step1_to_step2 : step step1 step2 := by validateStep step1 step2
+theorem step2_to_step3 : step step2 step3 := by validateStep step2 step3
+theorem step3_to_step4 : step step3 step4 := by validateStep step3 step4
+theorem step4_to_step5 : step step4 step5 := by validateStep step4 step5
+theorem step5_to_step6 : step step5 step6 := by validateStep step5 step6
+theorem step6_to_step7 : step step6 step7 := by validateStep step6 step7
+theorem step7_to_step8 : step step7 step8 := by validateStep step7 step8
+theorem step8_to_step9 : step step8 step9 := by validateStep step8 step9
+theorem step9_to_step10 : step step9 step10 := by validateStep step9 step10
+theorem step10_to_goal : step step10 goal := by validateStep step10 goal
 
 theorem init_to_goal : reachable init goal := by
   apply reachable.trans; exact init_to_step1
